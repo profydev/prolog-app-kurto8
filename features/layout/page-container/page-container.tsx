80247022ff12784b885221c18464d2b7d634d7ae
kurto8 <90794630+kurto8@users.noreply.github.com>
2023-03-02 18:09:08 -0700

    code clean up

--- features/layout/page-container/page-container.tsx
+++ features/layout/page-container/page-container.tsx
@@ -2,15 +2,7 @@ import React from "react";
 import Head from "next/head";
 import styled from "styled-components";
 import { SidebarNavigation } from "../sidebar-navigation";
-import {
-  color,
-  displayFont,
-  textFont,
-  space,
-  breakpoint,
-  theme,
-} from "@styles/theme";
-import MediaQuery from "react-responsive";
+import { color, displayFont, textFont, space, breakpoint } from "@styles/theme";
 import packageJson from "package.json";
 
 type PageContainerProps = {
