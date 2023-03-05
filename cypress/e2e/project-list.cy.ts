8507da2c12cf47a64dd0fdc1413af54d88b0be53
kurto8 <90794630+kurto8@users.noreply.github.com>
2023-03-04 17:38:41 -0700

    error message complete and cypress flow refactored

--- cypress/e2e/project-list.cy.ts
+++ cypress/e2e/project-list.cy.ts
@@ -6,60 +6,60 @@ describe("Project List", () => {
     cy.visit("http://localhost:3000/dashboard");
   });
 
-  it("renders a loading spinner while fetching data", () => {
-    cy.get('[data-cy="projects-loading-spinner"]').should("exist");
-  });
-
-  describe("unsuccesful fetching of data", () => {
-    beforeEach(() => {
-      // setup request mocks
-      cy.intercept("GET", "https://prolog-api.profy.dev/project", {
-        forceNetworkError: true,
-      })
-        .as("getProjectsFailure")
-        .wait(4000);
-    });
-
-    it("renders a error message if data is not fetched succesfully", () => {
-      cy.wait("@getProjectsFailure");
-      cy.get('[data-cy="error-fetching-projects"]').should("exist");
-    });
-  });
-
   describe("succesful fetching of data", () => {
     beforeEach(() => {
-      // setup request mocks
-      cy.intercept("GET", "https://prolog-api.profy.dev/project", {
-        fixture: "projects.json",
-      }).as("getProjects");
-    });
-
-    it("renders the projects", () => {
-      cy.wait("@getProjects");
-
-      const languageNames = ["React", "Node.js", "Python"];
-      const errorDisplayValue = ["Critical", "Warning", "Stable"];
-
-      // get all project cards
-      cy.get("main")
-        .find("li")
-        .each(($el, index) => {
-          // check that project data is rendered
-          cy.wrap($el).contains(mockProjects[index].name);
-          cy.wrap($el).contains(languageNames[index]);
-          cy.wrap($el).contains(mockProjects[index].numIssues);
-          cy.wrap($el).contains(mockProjects[index].numEvents24h);
-          cy.wrap($el).contains(errorDisplayValue[index]);
-          cy.wrap($el)
-            .find("a")
-            .should("have.attr", "href", "/dashboard/issues");
-        });
-    });
-  });
-
-  context("desktop resolution", () => {
-    beforeEach(() => {
       cy.viewport(1025, 900);
+    });
+
+    it("renders a loading spinner while fetching data", () => {
+      cy.get('[data-cy="projects-loading-spinner"]').should("exist");
+    });
+
+    describe("unsuccesful fetching of data", () => {
+      beforeEach(() => {
+        // setup request mocks
+        cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+          forceNetworkError: true,
+        })
+          .as("getProjectsFailure")
+          .wait(4000);
+      });
+
+      it("renders a error message if data is not fetched succesfully", () => {
+        cy.wait("@getProjectsFailure");
+        cy.get('[data-cy="error-fetching-projects"]').should("exist");
+      });
+    });
+
+    describe("succesful fetching of data", () => {
+      beforeEach(() => {
+        // setup request mocks
+        cy.intercept("GET", "https://prolog-api.profy.dev/project", {
+          fixture: "projects.json",
+        }).as("getProjects");
+      });
+
+      it("renders the projects", () => {
+        cy.wait("@getProjects");
+
+        const languageNames = ["React", "Node.js", "Python"];
+        const errorDisplayValue = ["Critical", "Warning", "Stable"];
+
+        // get all project cards
+        cy.get("main")
+          .find("li")
+          .each(($el, index) => {
+            // check that project data is rendered
+            cy.wrap($el).contains(mockProjects[index].name);
+            cy.wrap($el).contains(languageNames[index]);
+            cy.wrap($el).contains(mockProjects[index].numIssues);
+            cy.wrap($el).contains(mockProjects[index].numEvents24h);
+            cy.wrap($el).contains(errorDisplayValue[index]);
+            cy.wrap($el)
+              .find("a")
+              .should("have.attr", "href", "/dashboard/issues");
+          });
+      });
     });
   });
 });
